eb33d05f1a841485d3b4b79cfd0cf9aa873f2c9f
vinitkan <115861612+vinitkan@users.noreply.github.com>
2022-10-17 11:15:33 +0530

    Update Demo.java

--- GithubDemo/src/Demo.java
+++ GithubDemo/src/Demo.java
@@ -3,5 +3,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		 System.out.println("Hello Demo");
+		 System.out.println("Hello New  Demo");
+		 System.out.println("complete procces");
+		
 	}
 }
